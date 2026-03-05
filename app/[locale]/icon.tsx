81810bf8a97344cf4d093f73d9804b3a8be260e6
AnluYaens <anlujaen@gmail.com>
2026-03-05 22:45:20 +0100

    fix: move icon.tsx to [locale] segment to fix 404 from next-intl middleware

--- app/[locale]/icon.tsx
+++ app/[locale]/icon.tsx
@@ -1,7 +1,7 @@
 import { ImageResponse } from "next/og";
 
 export const runtime = 'edge';
-export const size = { width: 32, height: 32 };
+export const size = { width: 64, height: 64 };
 export const contentType = "image/png";
 
 export default function Icon() {
@@ -12,27 +12,21 @@ export default function Icon() {
         width: "100%",
         height: "100%",
         display: "flex",
-        flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
-        borderRadius: "7px",
+        borderRadius: "14px",
       }}
     >
       <span
         style={{
           color: "#FAFAF8",
-          fontSize: 11,
+          fontSize: 22,
           fontWeight: 700,
-          letterSpacing: -0.5,
+          letterSpacing: -1,
           lineHeight: 1,
         }}
       >
         ELN
-      </span>
-      <span
-        style={{ color: "#E8A0A0", fontSize: 8, lineHeight: 1, marginTop: 1 }}
-      >
-        ♥
       </span>
     </div>,
     { ...size },
