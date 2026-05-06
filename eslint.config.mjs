a3bb4ca6b8e4e8ce6c1d872aa90deeb7fb1e5d69
AnluYaens <anlujaen@gmail.com>
2026-05-06 17:49:17 +0200

    chore: upgrade to Next 16

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from "eslint/config";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
+
+export default defineConfig([{
+    ignores: [".next/**", "node_modules/**"],
+}, {
+    extends: [...nextCoreWebVitals, ...nextTypescript],
+    rules: {
+        "react-hooks/set-state-in-effect": "off",
+    },
+}]);
